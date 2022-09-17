6fe06272b4634a23b880cf763467848a5b233ab3
glaucia86 <glaucia_lemos86@hotmail.com>
2022-09-16 21:58:54 -0300

    important changes in the '01-storage.ps1' file

--- arm-azure-studies/demo-01/01-storage.ps1
+++ arm-azure-studies/demo-01/01-storage.ps1
@@ -2,6 +2,6 @@ $rg = 'arm-introduction-01'
 New-AzResourceGroup -Name $rg -Location northeurope -Force
 
 New-AzResourceGroupDeployment `
-  -Name 'new-storage'`
-  -ResourceGroupName $rg `
-  -TemplateFile '01-storage.json'
+    -Name 'new-storage' `
+    -ResourceGroupName $rg `
+    -TemplateFile '01-storage.json' 
